56cc1b3a731a4a4878daf076cac4ffae2f939ffe
lvyinger <21468329@qq.com>
2017-02-17 17:12:04 +0800

    Initial Commit

---
+++ PDTSimpleCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDTSimpleCalendar
+//
+//  Created by 吕万昌 on 2017/2/17.
+//  Copyright © 2017年 LWC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDTSimpleCalendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDTSimpleCalendar
+//
+//  Created by 吕万昌 on 2017/2/17.
+//  Copyright © 2017年 LWC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
